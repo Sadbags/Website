a599aabcf863f818b9caf0a7160bb6ee7d4b29c1
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-09-05 09:44:55 -0400

    adding app.py, database.py, and models

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from config import Config
+from database import db
 from models import db, MenuItem
 
 app = Flask(__name__)
-app.config.from_object(Config)
 
+db_name = 'restaurant_menu'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 @app.before_first_request
